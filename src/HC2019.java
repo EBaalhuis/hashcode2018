7d732b100daf7c8454e6aabee4db234aa195b5d7
erik <erik@erik-ThinkPad-L450>
2019-02-28 18:56:29 +0100

    read input

--- src/HC2019.java
+++ src/HC2019.java
@@ -34,8 +34,19 @@ public class HC2019 {
 	}
 
 	static class Instance {
-
+		public int n;
+		public Photo[] photos;
+		
 		Instance() {
+		}
+	}
+	
+	static class Photo {
+		public boolean h;
+		public HashSet<String> tags;
+		
+		Photo() {
+			tags = new HashSet<>();
 		}
 	}
 
@@ -47,6 +58,19 @@ public class HC2019 {
 			br = new BufferedReader(new FileReader(PATH + id + ".txt"));
 			String line = br.readLine();
 			String[] nrs = line.split(" ");
+			I.n = Integer.parseInt(nrs[0]);
+			I.photos = new Photo[I.n];
+			
+			for (int j = 0; j < I.n; j++) {
+				line = br.readLine();
+				String [] split = line.split(" ");
+				I.photos[j] = new Photo();
+				I.photos[j].h = split[0].equals("H");
+				int m = Integer.parseInt(split[1]);
+				for (int k = 0; k < split.length; k++) {
+					I.photos[j].tags.add(split[k+2]);
+				}
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
